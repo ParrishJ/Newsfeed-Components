8428b541f0c60e06924ff3ab51a1ddb85f2d40ec
ParrishJ <jarparrish@gmail.com>
2019-04-10 15:02:54 -0700

    Transition stretch goal complete

--- Article/Article.js
+++ Article/Article.js
@@ -13,7 +13,7 @@ class Article {
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-    this.domElement.classList.toggle("article-open")
+    this.domElement.classList.toggle("article-open") // Be careful about ".classList". Because its classList, we dont need a period in the parenthesis.
   }
 }
 
@@ -34,3 +34,6 @@ let articles = document.querySelectorAll(".article");
 articles.forEach(function(article){
   new Article(article);
 });
+
+
+
